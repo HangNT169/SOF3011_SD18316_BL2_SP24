f42a773f8fed56f6de146a09e35953398c18cd1d
HangNT169 <HangNT169@fpt.edu.vn>
2024-03-06 11:31:46 +0700

    Buổi 2: Làm việc với form

---
+++ src/main/java/com/sof3011/hangnt169/B2_LamViecVoiForm/LoginServlet.java
@@ -0,0 +1,41 @@
+package com.sof3011.hangnt169.B2_LamViecVoiForm;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+// VALUE LUON LUON BAT DAU BANG /
+// VALUE chua duong dan cua trinh duyet
+// BE & FE trao doi thong qua controller bang phuong thuc HTTP Methods
+// Co 4 loai co ban:
+// GET :  = READ : Hien thi du lieu (Lay ra) <=> SELECT
+// POST: <=> CREATE: Them du lieu <=> INSERT INTO
+// PUT : <=> UPDATE: Sua du lieu
+// DELETE <=> DELETE: Xoa du lieu
+// J4/ J5: Get , Post
+// GET: Hien thi lay ra
+// Post: Xu ly
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Truyen gia tri tu servlet sang jsp => setAttribute
+        String mess ="HangNT169";
+        request.setAttribute("mess123",mess);
+        // Chuyen trang
+        // C1: Mnag du lieu muon truyen sang jsp
+//        request.getRequestDispatcher("a.jsp").forward(request,response);
+        // C2:Chuyen trang => Chi chuyen trang
+        response.sendRedirect("a.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/a.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hangnt
+  Date: 06/03/2024
+  Time: 10:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="action_page.php" method="post">
+    ${mess123}
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+    </div>
+
+</form>
+</body>
+</html>
